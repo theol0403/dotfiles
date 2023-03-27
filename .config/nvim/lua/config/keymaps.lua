bcd76ecc71f352e9f90d0b5a9abae66d6b5328a5
Theo Lemay <theol0403@gmail.com>
2023-03-27 15:44:47 -0400

    save

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -60,23 +60,26 @@ imap("<C-BS>", "<C-w>")
 imap("jk", "<Esc>")
 imap("kj", "<Esc>")
 
+map("<C-S>", ":wall<CR><ESC>")
+imap("<C-S>", "<ESC>:wall<CR>")
+
 -- -- ------------------------- neovim specific config ----------------------------
--- if is_nvim() then
-nmap("<leader>w", "<c-w>")
---
--- -- ------------------------- vscode specific config ----------------------------
--- elseif is_vscode() then
--- 	nnoremap("gz", '<Cmd>call VSCodeNotifyRange("git.revertSelectedRanges", line("."), line("."), 0)<CR>')
--- 	xnoremap("gz", '<Cmd>call VSCodeNotifyRange("git.revertSelectedRanges", line("v"), line("."), 0)<CR><ESC>')
---
--- 	noremap("<leader>", '<Cmd>call VSCodeNotifyVisual("vspacecode.space", 1)<CR>')
---
--- 	-- nnoremap ('gt', '<Cmd>call VSCodeNotify("workbench.action.showAllEditors")<CR>')
--- 	-- nnoremap ('gT', '<Cmd>call VSCodeNotify("workbench.action.showAllEditorsByMostRecentlyUsed")<CR>')
--- 	nnoremap("go", '<Cmd>call VSCodeNotify("workbench.action.quickOpen")<CR>')
---
--- 	-- nnoremap (']c', '<Cmd>call VSCodeNotify("workbench.action.editor.nextChange")<CR>')
--- 	-- nnoremap ('[c', '<Cmd>call VSCodeNotify("workbench.action.editor.previousChange")<CR>')
--- 	-- nnoremap (']e', '<Cmd>call VSCodeNotify("editor.action.marker.next")<CR>')
--- 	-- nnoremap ('[e', '<Cmd>call VSCodeNotify("editor.action.marker.prev")<CR>')
--- end
+if is_nvim() then
+  nmap("<leader>w", "<c-w>")
+
+-- ------------------------- vscode specific config ----------------------------
+elseif is_vscode() then
+  nnoremap("gz", '<Cmd>call VSCodeNotifyRange("git.revertSelectedRanges", line("."), line("."), 0)<CR>')
+  xnoremap("gz", '<Cmd>call VSCodeNotifyRange("git.revertSelectedRanges", line("v"), line("."), 0)<CR><ESC>')
+
+  noremap("<leader>", '<Cmd>call VSCodeNotifyVisual("vspacecode.space", 1)<CR>')
+
+  -- nnoremap ('gt', '<Cmd>call VSCodeNotify("workbench.action.showAllEditors")<CR>')
+  -- nnoremap ('gT', '<Cmd>call VSCodeNotify("workbench.action.showAllEditorsByMostRecentlyUsed")<CR>')
+  nnoremap("go", '<Cmd>call VSCodeNotify("workbench.action.quickOpen")<CR>')
+
+  -- nnoremap (']c', '<Cmd>call VSCodeNotify("workbench.action.editor.nextChange")<CR>')
+  -- nnoremap ('[c', '<Cmd>call VSCodeNotify("workbench.action.editor.previousChange")<CR>')
+  -- nnoremap (']e', '<Cmd>call VSCodeNotify("editor.action.marker.next")<CR>')
+  -- nnoremap ('[e', '<Cmd>call VSCodeNotify("editor.action.marker.prev")<CR>')
+end
