e038ff73b784ef4a9ff96aece54b2c4b6edf1ef2
Theo Lemay <tlemay@tesla.com>
2025-05-12 10:00:39 -0700

    macos

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -5,13 +5,13 @@ vim.opt.gdefault = true
 vim.opt.inccommand = "split"
 vim.o.clipboard = ""
 
-vim.o.guifont = "JetBrainsMono Nerd Font:h10"
+vim.o.guifont = "JetBrainsMono Nerd Font:h11"
 vim.g.neovide_hide_mouse_when_typing = true
 vim.g.experimental_layer_grouping = true
 vim.g.neovide_cursor_smooth_blink = true
 vim.g.neovide_remember_window_size = true
-vim.g.neovide_text_gamma = 0.8
-vim.g.neovide_text_contrast = 0.1
+-- vim.g.neovide_text_gamma = 0.8
+-- vim.g.neovide_text_contrast = 0.1
 
 vim.opt.guicursor:append("t:ver25")
 
